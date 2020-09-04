22a29a6378b866069a4fb23fd00888e57db8d1a7
Spencer <Spencer@RAZERBLADE01>
2020-09-04 14:22:03 -0500

    Fixed logic in if statement

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -37,8 +37,7 @@ public class KaprekarUtils {
 			modulus *= 10;
 			first = square / modulus;
 			second = square % modulus;
-			if(first > 0 &&
-			   first + second = n) {
+			if(second > 0 && first + second == n) {
 				return true;
 			}
 		}
