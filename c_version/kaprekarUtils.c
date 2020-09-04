c062217cdfcd0d847ba7049cb375919c185a2804
snicha2020 <sallen35@huskers.unl.edu>
2020-09-04 19:09:11 +0000

    Fixed variable name typos

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -37,9 +37,9 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
